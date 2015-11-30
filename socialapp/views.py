a2358046c35b203f05853416bc13e547fb178daf
dan-ghita <dan_ghita_95@yahoo.com>
2015-11-30 16:16:10 +0200

    Tema 4

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from socialapp.models import UserPost
-from socialapp.forms import UserPostForm
+from socialapp.models import UserPost, UserPostComment
+from socialapp.forms import UserPostForm, UserPostCommentForm
 
 
 def index(request):
@@ -23,6 +23,18 @@ def index(request):
 
 
 def post_details(request, pk):
+    if request.method == 'POST':
+        form = UserPostCommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            comment = UserPostComment(text=text)
+            comment.post_id = pk
+            comment.save()
+
     post = UserPost.objects.get(pk=pk)
-    context = {'post': post}
+    comments = post.userpostcomment_set.all().order_by('-date_added')
+    form = UserPostCommentForm()
+
+    context = {'post': post, 'comments': comments, 'form': form}
+
     return render(request, 'post_details.html', context)
